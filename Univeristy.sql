522b43daa955fa47fd171c247374d89fd7360f64
pkrgupta <48147551+pkrgupta@users.noreply.github.com>
2019-04-29 12:50:36 -0700

    Update Univeristy.sql

--- Univeristy.sql
+++ Univeristy.sql
@@ -1,54 +1,60 @@
-CREATE DATABASE University;
+--CREATE DATABASE University;
+CREATE DATABASE SCHEDULE;
 
-USE University;
+--USE University;
+USE SCHEDULE;
 
 --Location Entity
-CREATE TABLE Location(
-	LocationID VARCHAR(10) NOT NULL,
-	BuildingName VARCHAR(20),
-	Campus VARCHAR(20),
-	CONSTRAINT Location_PK PRIMARY KEY (LocationID)
+CREATE TABLE Location (
+	LocationID VARCHAR(10)
+	,BuildingName VARCHAR(20) NOT NULL
+	,RoomNumber INT NOT NULL
+	,Floor INT NOT NULL
+	,Campus VARCHAR(20) CHECK (Campus in ('Hayward','Concord','Online'))
+	,CONSTRAINT Location_PK PRIMARY KEY (LocationID)
 );
 
 --PersonType Entity is to implement inheritance 
-CREATE TABLE PersonType(
-	TypeID int,
-	personType varchar(10) NOT NULL,
-	CONSTRAINT PersonType_PK PRIMARY KEY (TypeID)
+CREATE TABLE PersonType (
+	TypeID INT CHECK (TypeID in ('S','I')
+	,PersonType CHAR(7) (PersonType in ('Student','Instructor')
+	,CONSTRAINT PersonType_PK PRIMARY KEY (TypeID)
 );
 
--- Supertype: People Entity
-CREATE TABLE People(
-	NetID CHAR(6) NOT NULL,
-	TypeID INT NOT NULL,
-	PName VARCHAR(30) NOT NULL,
-	Email VARCHAR(30),
-	CONSTRAINT People_PK PRIMARY KEY (NetID),
-	CONSTRAINT People_FK FOREIGN KEY (TypeID) REFERENCES PersonType(TypeID),
-	CONSTRAINT People_AltPK UNIQUE (NetID, TypeID)
+-- SuperType: People Entity
+CREATE TABLE People (
+	NetID CHAR(6)
+	,TypeID CHAR(1) NOT NULL
+	,PFirstName VARCHAR(30) NOT NULL
+	,PLastName VARCHAR(20) NOT NULL
+	,Email VARCHAR(40) NOT NULL
+	,CONSTRAINT People_PK PRIMARY KEY (NetID)
+	,CONSTRAINT People_FK FOREIGN KEY (TypeID) REFERENCES PersonType(TypeID)
+--	,CONSTRAINT People_AltPK UNIQUE (NetID, TypeID)
 );
 
 -- Subtype: Student Entity
 CREATE TABLE Student(
-	NetID CHAR(6) NOT NULL,
-	TypeID INT NOT NULL DEFAULT 1 CHECK (TypeID = 1), --TypeID =1 stands for Student
-	Major VARCHAR(20),
-	GraducationSemester VARCHAR(10),
-	CONSTRAINT Student_PK PRIMARY KEY (NetID),
-	CONSTRAINT Student_FK FOREIGN KEY (NetID, TypeID) REFERENCES People(NetID, TypeID)
-)
+	NetID CHAR(6) NOT NULL
+	,TypeID INT DEFAULT 'S' CHECK (TypeID in ('S'))
+	,Major VARCHAR(20) NOT NULL
+	,GraducationSemester VARCHAR(10) CHECK (GraducationSemester in ('Spring','Summer','Fall','Winter')) NOT NULL
+	,GraducationYear INT NOT NULL					  
+	,CONSTRAINT Student_PK PRIMARY KEY (NetID)
+--	,CONSTRAINT Student_FK FOREIGN KEY (NetID, TypeID) REFERENCES People(NetID, TypeID)
+);
 -- Subtype: Instructor Entity
-CREATE TABLE Instructor(
-	NetID CHAR(6) NOT NULL,
-	TypeID INT NOT NULL DEFAULT 2 CHECK (TypeID = 2), --TypeID =2 stands for Instructor
-	InstructorOffice VARCHAR(10),
-	CONSTRAINT Instructor_PK PRIMARY KEY (NetID),
-	CONSTRAINT Instructor_FK FOREIGN KEY (NetID, TypeID) REFERENCES People(NetID, TypeID),
-	CONSTRAINT Instructor_FK1 FOREIGN KEY (InstructorOffice) REFERENCES Location(LocationID)
+CREATE TABLE Instructor (
+	NetID CHAR(6) NOT NULL
+	,TypeID INT DEFAULT 'I' CHECK (TypeID in ('I'))
+	,InstructorOffice VARCHAR(10)
+	,CONSTRAINT Instructor_PK PRIMARY KEY (NetID)
+	,CONSTRAINT Instructor_FK FOREIGN KEY (InstructorOffice) REFERENCES Location(LocationID)
+--	,CONSTRAINT Instructor_FK1 FOREIGN KEY (NetID, TypeID) REFERENCES People(NetID, TypeID)
 );
 
 -- Course Table and have unary relationship
-CREATE TABLE Course(
+CREATE TABLE Course (
 	Course# VARCHAR(10) NOT NULL,
 	Prerequisite VARCHAR(10),
 	Prerequisite_2 VARCHAR (10),
