d70a272c6eaf15385134beefecd14e11766984c0
pkrgupta <48147551+pkrgupta@users.noreply.github.com>
2019-04-30 11:00:56 -0700

    Update Univeristy.sql

--- Univeristy.sql
+++ Univeristy.sql
@@ -10,14 +10,14 @@ CREATE TABLE Location (
 	,BuildingName VARCHAR(20) NOT NULL
 	,RoomNumber INT NOT NULL
 	,Floor INT NOT NULL
-	,Campus VARCHAR(20) CHECK (Campus in ('Hayward','Concord','Online'))
+	,Campus VARCHAR(20) CHECK (Campus in ('Hayward','Concord','Online')) NOT NULL
 	,CONSTRAINT Location_PK PRIMARY KEY (LocationID)
 );
 
 --PersonType Entity is to implement inheritance 
 CREATE TABLE PersonType (
-	TypeID INT CHECK (TypeID in ('S','I')
-	,PersonType CHAR(7) (PersonType in ('Student','Instructor')
+	TypeID INT CHECK (TypeID in ('S','I') 
+	,PersonType CHAR(7) (PersonType in ('Student','Instructor') NOT NULL
 	,CONSTRAINT PersonType_PK PRIMARY KEY (TypeID)
 );
 
@@ -28,16 +28,16 @@ CREATE TABLE People (
 	,PFirstName VARCHAR(30) NOT NULL
 	,PLastName VARCHAR(20) NOT NULL
 	,Email VARCHAR(40) NOT NULL
-	,Gender CHAR(1) CHECK (Gender IN ('M','F','O')) 
+	,Gender CHAR(1) CHECK (Gender IN ('M','F','O')) NOT NULL
 	,CONSTRAINT People_PK PRIMARY KEY (NetID)
 	,CONSTRAINT People_FK FOREIGN KEY (TypeID) REFERENCES PersonType(TypeID)
 --	,CONSTRAINT People_AltPK UNIQUE (NetID, TypeID)
 );
 
 -- Subtype: Student Entity
-CREATE TABLE Student(
-	NetID CHAR(6) NOT NULL
-	,TypeID INT DEFAULT 'S' CHECK (TypeID in ('S'))
+CREATE TABLE Student (
+	NetID CHAR(6) 
+	,TypeID INT DEFAULT 'S' CHECK (TypeID in ('S')) NOT NULL
 	,Major VARCHAR(20) NOT NULL
 	,GraducationSemester VARCHAR(10) CHECK (GraducationSemester in ('Spring','Summer','Fall','Winter')) NOT NULL
 	,GraducationYear INT NOT NULL					  
@@ -47,20 +47,20 @@ CREATE TABLE Student(
 
 -- Subtype: Instructor Entity
 CREATE TABLE Instructor (
-	NetID CHAR(6) NOT NULL
-	,TypeID INT DEFAULT 'I' CHECK (TypeID in ('I'))
-	,InstructorOffice VARCHAR(10)
-	,CONSTRAINT Instructor_PK PRIMARY KEY (NetID)
+	NetID CHAR(6) 
+	,TypeID INT DEFAULT 'I' CHECK (TypeID in ('I')) NOT NULL
+	,InstructorOffice VARCHAR(10) NOT NULL
+	,CONSTRAINT Instructor_PK PRIMARY KEY (NetID) 
 	,CONSTRAINT Instructor_FK FOREIGN KEY (InstructorOffice) REFERENCES Location(LocationID)
 --	,CONSTRAINT Instructor_FK1 FOREIGN KEY (NetID, TypeID) REFERENCES People(NetID, TypeID)
 );
 
--- Course Table and have unary relationship
+-- Course Entity with Unary relationship
 CREATE TABLE Course (
-	CourseID VARCHAR(10) NOT NULL
-	,CourseName VARCHAR(40)
-	,CreditHours DECIMAL(2,1)
-	,Desciption TEXT
+	CourseID VARCHAR(10) 
+	,CourseName VARCHAR(40) NOT NULL
+	,CreditHours REAL NOT NULL
+	,Desciption TEXT 
 	,PreRequisite_1 VARCHAR(10)
 	,PreRequisite_2 VARCHAR(10)
 	,CONSTRAINT Course_PK PRIMARY KEY (CourseID)
@@ -69,7 +69,7 @@ CREATE TABLE Course (
 );
 
 
---CourseBookList Entity
+--Publisher Entity
 CREATE TABLE Publisher (
 	PublisherID INT
 	,PublisherName VARCHAR(30) NOT NULL
@@ -79,60 +79,75 @@ CREATE TABLE Publisher (
 	,CONSTRAINT Publisher_PK PRIMARY KEY (PublisherID)
 );
 				       
---CourseBookList Entity
+--Author Entity
 CREATE TABLE Author (
 	AuthorID INT
-	,AuthorFirstName VARCHAR(30)
-	,AuthorLastName VARCHAR(20)
-	,AuthorGender CHAR(1) CHECK (AuthorGender in ('M','F','O'))
-	,AuthorCountry VARCHAR(20)
+	,AuthorFirstName VARCHAR(30) NOT NULL
+	,AuthorLastName VARCHAR(20) 
+	,AuthorGender CHAR(1) CHECK (AuthorGender in ('M','F','O')) NOT NULL
+	,AuthorCountry VARCHAR(20) NOT NULL
 	,CONSTRAINT Author_PK PRIMARY KEY (AuthorID)
 );
 				       
 				       
---CourseBookList Entity
-CREATE TABLE CourseBookList (
+--BookList Entity
+CREATE TABLE BookList (
 	ISBN INT
 	,BookName VARCHAR(100) NOT NULL
 	,Edition VARCHAR(2) NOT NULL
 	,EditionYear INT NOT NULL
-	,AuthorID VARCHAR(30) NOT NULL
+	,AuthorID_1 VARCHAR(30) NOT NULL
+	,AuthorID_2 VARCHAR(30) 
+	,AuthorID_3 VARCHAR(30) 
+	,AuthorID_4 VARCHAR(30) 
 	,PublisherID INT NOT NULL
-	,CONSTRAINT CourseBookList_PK PRIMARY KEY (ISBN)
-	,CONSTRAINT CourseBook_FK1 FOREIGN KEY (AuthorID) REFERENCES Author(AuthorID)
-	,CONSTRAINT CourseBook_FK2 FOREIGN KEY (PublisherID) REFERENCES Publisher(PublisherID)
+	,CONSTRAINT BookList_PK PRIMARY KEY (ISBN)
+	,CONSTRAINT Book_FK1 FOREIGN KEY (AuthorID_1) REFERENCES Author(AuthorID)
+	,CONSTRAINT Book_FK2 FOREIGN KEY (AuthorID_2) REFERENCES Author(AuthorID)
+	,CONSTRAINT Book_FK3 FOREIGN KEY (AuthorID_3) REFERENCES Author(AuthorID)
+	,CONSTRAINT Book_FK4 FOREIGN KEY (AuthorID_4) REFERENCES Author(AuthorID)
+	,CONSTRAINT Book_FK5 FOREIGN KEY (PublisherID) REFERENCES Publisher(PublisherID)
 );
        
 
 -- ClassList Entity
-CREATE TABLE ClassList(
-	Class# INT NOT NULL,
-	Semester VARCHAR(10) NOT NULL,
-	Course# VARCHAR(10),
-	SectionNo INT,
-	InstructorID CHAR(6),
-	MeetingDays VARCHAR(10),
-	StartsTime TIME,
-	EndsTime TIME,
-	CourseClassRoom VARCHAR(10),
-	CourseBook INT,
-	CourseBook_2 INT,
-	CONSTRAINT ClassList_PK PRIMARY KEY (Class#, Semester),
-	CONSTRAINT ClassList_FK FOREIGN KEY (Course#) REFERENCES Course(Course#),
-	CONSTRAINT ClassList_FK1 FOREIGN KEY (InstructorID) REFERENCES Instructor(NetID),
-	CONSTRAINT ClassList_FK2 FOREIGN KEY (CourseClassRoom) REFERENCES Location(LocationID),
-	CONSTRAINT ClassList_FK3 FOREIGN KEY (CourseBook) REFERENCES CourseBookList(ISBN),
-	CONSTRAINT ClassList_FK4 FOREIGN KEY (CourseBook_2) REFERENCES CourseBookList(ISBN)
+CREATE TABLE ClassList (
+	ClassID INT NOT NULL
+	,ClassSemester VARCHAR(10) CHECK (ClassSemester in ('Spring','Summer','Fall','Winter')) NOT NULL
+	,ClassYear INT NOT NULL
+	,CourseID VARCHAR(10) NOT NULL
+	,SectionNo INT NOT NULL
+	,InstructorID CHAR(6) NOT NULL
+	,MeetingDay1 VARCHAR(10) NOT NULL
+	,StartTime1 TIME NOT NULL
+	,EndTime1 TIME NOT NULL
+	,MeetingDay2 VARCHAR(10)
+	,StartTime2 TIME
+	,EndTime2 TIME
+	,MeetingDay3 VARCHAR(10)
+	,StartTime3 TIME
+	,EndTime3 TIME
+	,MeetingDay4 VARCHAR(10)
+	,StartTime4 TIME
+	,EndTime4 TIME
+	,ClassRoom VARCHAR(10) NOT NULL
+	,Book_1 INT  
+	,Book_2 INT
+	,CONSTRAINT ClassList_PK PRIMARY KEY (ClassID, SectionNo)
+	,CONSTRAINT ClassList_FK1 FOREIGN KEY (CourseID) REFERENCES Course(CourseID)
+	,CONSTRAINT ClassList_FK2 FOREIGN KEY (InstructorID) REFERENCES Instructor(NetID)
+	,CONSTRAINT ClassList_FK3 FOREIGN KEY (ClassRoom) REFERENCES Location(LocationID)
+	,CONSTRAINT ClassList_FK4 FOREIGN KEY (Book_1) REFERENCES BookList(ISBN)
+	,CONSTRAINT ClassList_FK5 FOREIGN KEY (Book_2) REFERENCES BookList(ISBN)
 );
 
 --Enrollment Entity
-CREATE TABLE Enrollment(
-	StudentID CHAR(6) NOT NULL,
-	Class# INT NOT NULL,
-	Semester VARCHAR(10) NOT NULL,
-	LetterGrade CHAR(2),
-	CONSTRAINT Enrollment_PK PRIMARY KEY (StudentID,Class#),
-	CONSTRAINT Enrollment_FK FOREIGN KEY (StudentID) REFERENCES Student(NetID),
-	CONSTRAINT Enrollment_FK1 FOREIGN KEY (Class#, Semester) REFERENCES ClassList(Class#,Semester)
+CREATE TABLE Enrollment (
+	StudentID CHAR(6) NOT NULL
+	,ClassID INT NOT NULL
+	,LetterGrade CHAR(2)
+	,CONSTRAINT Enrollment_PK PRIMARY KEY (StudentID,ClassID)
+	,CONSTRAINT Enrollment_FK FOREIGN KEY (StudentID) REFERENCES Student(NetID)
+	,CONSTRAINT Enrollment_FK1 FOREIGN KEY (ClassID,SectionNo) REFERENCES ClassList(ClassID,SectionNo)
 );
 
